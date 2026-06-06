aa37bbad123959ed0180a66c94aa51e40eb7a5e6
harukahikarii <30721068+harukahikarii@users.noreply.github.com>
2026-06-06 11:53:02 +0200

    sth

--- Scenes/main_menu.gd
+++ Scenes/main_menu.gd
@@ -13,7 +13,7 @@ func _process(delta: float) -> void:
 
 func _on_start_pressed() -> void:
 	print("start pressed")
-	#get_tree().change_scene_to_file()
+	get_tree().change_scene_to_file("res://Scenes/game_scene.tscn")
 
 
 func _on_options_pressed() -> void:
