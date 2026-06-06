3a39e92c4706a0eabf1e97a7d755129d7520a80f
tessi <201974491+TessiDoesCS@users.noreply.github.com>
2026-06-06 17:29:52 +0200

    this workey?

--- Sprites/Menus/end_scene_script.gd
+++ Sprites/Menus/end_scene_script.gd
@@ -2,7 +2,9 @@ extends Node2D
 
 @export var animatedSprite : AnimatedSprite2D
 
+signal go_to_lb()
+
 func _ready():
 	await get_tree().create_timer(5.0).timeout
-	get_tree().change_scene_to_file("res://Scenes/leaderboard_scene.tscn")
+	go_to_lb.emit()
 	pass
