7ab92a697840f9069617fadc426f59f3b0de9ed1
Abdullah Al-Hatem <abhatem@Abdullahs-MacBook-Air.local>
2026-06-06 20:54:13 +0200

    button sounds and animations

--- Scripts/QTEs/KeyQTE/key_qte.gd
+++ Scripts/QTEs/KeyQTE/key_qte.gd
@@ -1,6 +1,9 @@
 class_name KeyQTE extends QTEBase
 
 var key: int = [KEY_W, KEY_A, KEY_S, KEY_D].pick_random()
+var done: bool = false
+@export var success_sounds: Array[AudioStream]
+
 
 func _ready():
 	# yes, im lazy :P
@@ -41,8 +44,29 @@ func _unhandled_input(event):
 func check_event(event):
 	if event is InputEventKey and event.pressed and not event.is_echo():
 		if event.key_label == key:
+			if done:
+				return
+			
+			done = true
+			
 			# yes, im lazy :P
-			get_parent().get_parent().get_parent().remove_key_qte(self)
 			get_viewport().set_input_as_handled()
+			
+			$FailTimer.stop()
+			
+			$SuccessSound.stream = success_sounds.pick_random()
+
+			#var original_length = $SuccessSound.stream.get_length()
+			#$SuccessSound.pitch_scale = 1.0
+
+			$SuccessSound.play()
+			
+			var tween = create_tween()
+			tween.set_trans(Tween.TRANS_SINE)
+			tween.tween_property($KeySprite, "position:y", $KeySprite.position.y + 5, 0.1)
+			
+			await get_tree().create_timer(0.5).timeout
+			
+			get_parent().get_parent().get_parent().remove_key_qte(self)
 			QTE_succeded.emit(position)
 			queue_free()
--- Scripts/QTEs/MashQTE/mash_qte.gd
+++ Scripts/QTEs/MashQTE/mash_qte.gd
@@ -1,6 +1,9 @@
 class_name MashQTE extends KeyQTE
 
 var mash_amount: int = 10
+var initial_mash_amount: int
+var active_tween: Tween
+@export var click_sound : AudioStream
 
 func _ready():
 	# yes, im lazy :P
@@ -23,12 +26,46 @@ func check_event(event):
 	if event is InputEventKey and event.pressed and not event.is_echo():
 		if event.key_label == key:
 			get_viewport().set_input_as_handled()
+			
+			
+			#$SuccessSound.stream = success_sounds.pick_random()
+#
+			#$SuccessSound.play()
+			
+			if not done:
+				mash_amount -= 1
+				$NumberLabel.text = str(mash_amount)
+			
+			if active_tween and active_tween.is_valid():
+				active_tween.kill()
+				
+			active_tween = create_tween()
+			$KeySprite.scale = Vector2(2.0, 2.0) 
+			active_tween.tween_property($KeySprite, "scale", Vector2(1.7, 1.7), 0.15).set_trans(Tween.TRANS_SPRING)
+			
+			if mash_amount > 0:
+				# Calculate progress from 0.0 to 1.0
+				var progress = 1.0 - (float(mash_amount) / float(initial_mash_amount))
+				
+				# Increase pitch from 1.0 up to 2.0 as they get closer to 0
+				
+				$SuccessSound.pitch_scale = lerp(1.0, 2.0, progress)
+				$SuccessSound.stream = click_sound
+				$SuccessSound.play()
+			else:
+				if done:
+					return
+				done = true
+				# --- FINISHED ---
+				$SuccessSound.pitch_scale = 1.0 
+				$SuccessSound.stream = success_sounds.pick_random()
+				$SuccessSound.play()
+				var tween = create_tween()
+				tween.set_trans(Tween.TRANS_SINE)
+				tween.tween_property($KeySprite, "position:y", $KeySprite.position.y + 5, 0.1)
+			
+				await get_tree().create_timer(0.5).timeout
 
-			mash_amount -= 1
-			$NumberLabel.text = str(mash_amount)
-
-			if mash_amount <= 0:
-				# yes, im lazy :P
 				get_parent().get_parent().get_parent().remove_key_qte(self)
 				DifficultyDirector.start_input_cooldown(0.25)
 				QTE_succeded.emit(position)
