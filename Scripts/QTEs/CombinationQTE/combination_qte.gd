c6742e501618ce807c8a79694e229258cedbf05e
Abdullah Al-Hatem <abhatem@Abdullahs-MacBook-Air.local>
2026-06-07 03:35:18 +0200

    Combo button sound and animation

--- Scripts/QTEs/CombinationQTE/combination_qte.gd
+++ Scripts/QTEs/CombinationQTE/combination_qte.gd
@@ -3,6 +3,7 @@ class_name CombinationQTE extends KeyQTE
 @export var key_sequence: Array[Key] = [KEY_A, KEY_D, KEY_A, KEY_D]
 @export var first_step_wait_time: float = 7.0
 @export var follow_up_step_wait_time: float = 2.0
+@export var click_sound: AudioStream
 var current_step: int = 0
 
 func _ready():
@@ -35,15 +36,30 @@ func advance_sequence():
 		return
 
 	current_step += 1
+
+	
+
 	
 	if current_step >= key_sequence.size():
+		$SuccessSound.stream = success_sounds.pick_random()
+
+		$SuccessSound.play()	
+		var tween = create_tween()
+		tween.set_trans(Tween.TRANS_SINE)
+		tween.tween_property($KeySprite, "position:y", $KeySprite.position.y + 5, 0.1)
+		await get_tree().create_timer(0.5).timeout
 		is_resolved = true
 		unregister_key_qte()
 		$FailTimer.stop()
 		DifficultyDirector.start_input_cooldown(0.25)
 		QTE_succeded.emit(position)
 		queue_free()
+		
 	else:
+		var progress = 1.0 - (float(current_step) / float(maxi(key_sequence.size(), 1)))
+		$SuccessSound.pitch_scale = maxf(0.01, lerp(1.0, 2.0, clampf(progress, 0.0, 1.0)))
+		$SuccessSound.stream = click_sound
+		$SuccessSound.play()
 		key = key_sequence[current_step]
 		_start_step_timer()
 		update_ui()
